55cedb94a500e5d04ded5ef7d5ffac9ee9879911
threethousandmonkeys <nicole.cheung7171@gmail.com>
2018-10-16 17:29:52 +1100

    update Route, checkWall

--- src/mycontroller/MyAIController.java
+++ src/mycontroller/MyAIController.java
@@ -5,14 +5,25 @@ import utilities.Coordinate;
 import world.Car;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import controller.CarController;
+import tiles.HealthTrap;
+import tiles.LavaTrap;
+import tiles.MapTile;
+
 
 public class MyAIController extends CarController{
 	// to store how many keys already got
     private ArrayList<Coordinate> keyList = new ArrayList<>();
+    private HashMap<Coordinate, MapTile> map = super.getMap();
+    private Route route;
 
 	public MyAIController(Car car) {
 		super(car);
+        route = new Route(map, super.getPosition());
 	}
+
+
 
 	@Override
 	public void update() {
