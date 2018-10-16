f1caf576a1a120742974df91ee4a0075ba5f2c4b
threethousandmonkeys <nicole.cheung7171@gmail.com>
2018-10-17 10:44:40 +1100

    update Route, checkWall, and TODO

--- src/mycontroller/MyAIController.java
+++ src/mycontroller/MyAIController.java
@@ -1,5 +1,6 @@
 package mycontroller;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import controller.CarController;
 import utilities.Coordinate;
 import world.Car;
@@ -23,12 +24,27 @@ public class MyAIController extends CarController{
         route = new Route(map, super.getPosition());
 	}
 
-
-
+	/**
+     * basic idea: view and gain the TRAP information
+     * then use the info to recover the original map
+     * finally use DFS/Dijkstra to get all keys then exit.
+    */
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		exploreMap(super.getView());
 	}
 
+	// explore and recover the original map based on the TRAP info received.
+	public void exploreMap(HashMap<Coordinate, MapTile> knownMap){
+	    for(Coordinate coord : knownMap.keySet()){
+	        MapTile current = knownMap.get(coord);
+	        if(current.isType(MapTile.Type.TRAP)){
+                //TODO: DO something to handle the trap, and mark it as handled
+            }
+        }
+    }
+
+    public void handleTheTrap(Coordinate coord, MapTile mapTile){
+	    //TODO: if there is a LAVA, based on the condition(do we have any keys?)
+    }
 }
